8c9c0a7b444d0ec089d95193c88649d21c92c7f4
Takondwa Kakusa <tk421@vt.edu>
2017-02-12 18:58:11 +0000

    Changed the Client to the new version

--- Client.py
+++ Client.py
@@ -1,13 +1,39 @@
-#!/usr/bin/env python3
-
-"""
-Client for assignment 1
-"""
-
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy.streaming import StreamListener
+import pickle
 import socket
+import json
+import re
 import sys
-import Payload as pl
+import unicodedata
+import hashlib
 
+ckey =	'DhZMPy4JdoHXE0fjoaZq8UESZ'
+csecret = 'xd5XanwZ1dn2OMpO9aBrEI7XKiw2XaZyhYpnsUly5Db5ol797t'
+atoken = '4856679433-ZvQquWCdv4TftB13vCmcrn88gWSWe2iBscbf00o'
+asecret = '3QT3g5tV4XjbgxxCDsd7YobwfhGDTxenYvmGtCYSvst0v'
+question = ''
+
+#sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sender.connect(('172.25.19.231', 2000))
+
+def getPayload(text):
+    tup = (text, hashlib.md5(text.encode('utf-8')).hexdigest())
+    payload = pickle.dumps(tup)
+    return payload
+
+
+def getText(payload):
+    tup = pickle.loads(payload)
+    text = tup[0]
+    transmittedCheck = tup[1]
+    calculatedCheck = hashlib.md5(text.encode('utf-8')).hexdigest()
+    if transmittedCheck != calculatedCheck:
+        print("Error unpacking payload: calculated checksum did not match received checksum")
+        return None
+    return text
 
 def getSocket(address, port):
     s = None
@@ -24,15 +50,15 @@ def getSocket(address, port):
 
 def getAnswer(question, address, port):
     soc = getSocket(address, port)
-    soc.send(pl.getPayload(question))
+    soc.send(getPayload(question))
     answers = []
     answerPayload = soc.recv(1024)
-    answertext = pl.getText(answerPayload)
+    answertext = getText(answerPayload)
     answers.append(answertext)
     try:
         while 1:
             answerPayload = soc.recv(1024)
-            answertext = pl.getText(answerPayload)
+            answertext = getText(answerPayload)
             answers.append(answertext)
     except:
         soc.close()
@@ -42,5 +68,39 @@ def getAnswer(question, address, port):
         print("Error receiving answer")
         return None
 
-print('Received:', getAnswer("Here's the first question", 'localhost', 50000))
-print('Received:', getAnswer("Here's the second question", 'localhost', 50000))
+def messageParser(line):
+    tweeter = re.search('(?<=@)\w+', line)
+    ipaddr = re.search('(?<=#)[0-9.]*', line)
+    portaddr = re.search('(?<=:)[0-9]*', line)
+    message = re.search('(?<=\_).*', line)
+    message2 = re.sub('\”','',message.group(0))
+    message3 = re.sub('\"','',message2)
+    return (tweeter, ipaddr.group(0), portaddr.group(0), message3)
+    
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        line = json.loads(data)
+        (tweeter, ipaddr, portaddr, message) = messageParser(line['text'])
+        question = message
+        print ('got the message ', question)
+        print ('Got IP Address  ', ipaddr)
+        answer = getAnswer(str(question), str(ipaddr), int(portaddr))
+        print ('Got the Answer')
+        for tweet in answer:
+            api.update_status('@drewrepp Team_03 "' + tweet +'"')
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+api = API(auth)
+twitterStream = Stream(auth, listener())
+question = twitterStream.filter(track=["tomjones356"])
+while (1):
+    if (question != ''):
+        print (question)
+        question = ''
