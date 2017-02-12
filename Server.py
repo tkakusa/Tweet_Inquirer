16f76798f52a4780b592e9e4024fd3f5663dffa3
Takondwa Kakusa <tk421@vt.edu>
2017-02-12 13:35:21 -0500

    Added wolfram to the server code

--- Server.py
+++ Server.py
@@ -5,6 +5,7 @@ Server for assignment 1
 """
 import pickle
 import hashlib
+import wolframalpha
 import socket
 import sys
 
@@ -23,6 +24,42 @@ def getText(payload):
         print("Error unpacking payload: calculated checksum did not match received checksum")
         return None
     return text
+
+def wolframQuery(query):
+    try:
+        # create wolfram alpha client service
+        wolframService = wolframalpha.Client("9KL76E-2P62RRAAJ2")
+        print("[DBG] Asking Wolfram the question.")
+
+        # send the question through the wolfram alpha api
+        answerPackage = wolframService.query(query)
+    except Exception:
+        return ["[ERR] There was a problem connecting to Wolfram Alpha."]
+    else:
+        print("[DBG] Got answer back.")
+
+        # get answer from wolfram alpha
+        try:
+            results, = answerPackage.results
+        except AttributeError:
+            return ["[ERR] Wolfram Alpha did not understand your query."]
+        else:
+            print("[DBG] Extracting results.")
+            # Extract the actual text of the answer
+            answer = results.text
+
+            # Split multiple answers
+            answerList = re.split("\n[0-9] \| ", answer)
+
+            # Put the numbers back on the front of the multiple answers that split() removed
+            i = 1
+            for multAnswer in answerList:
+                if(i > 1):
+                    answerList[i - 1] = (str(i) + " | " + multAnswer)
+                i = i + 1;
+
+            # Return the complete list of answers
+            return answerList
 
 
 host = ''
